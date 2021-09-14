7ccb58da3fbce11bd79a6c074719cd52a51062ee
Lotus1438 <reinamates01@gmail.com>
2021-09-14 15:37:05 +0800

    crud ui

--- frontend/src/views/Questionaire.vue
+++ frontend/src/views/Questionaire.vue
@@ -1,19 +1,12 @@
 <template>
   <div>
     <b-card class="card" title="Create New Question">
-      <!-- <b-card-text>All the best for your new project.</b-card-text>
-      <b-card-text>Please make sure to read our <b-link
-        href="https://pixinvent.com/demo/vuexy-vuejs-admin-dashboard-template/documentation/"
-        target="_blank"
-      >
-        Template Documentation
-      </b-link> to understand where to go from here and how to use our template.</b-card-text> -->
       <b-form-textarea
       id="textarea"
       v-model="ask"
       placeholder="Create question..."
       rows="3"
-      max-rows="6">
+      max-rows="">
       </b-form-textarea>
       <!-- multiple choice  -->
       <b-form-radio-group
@@ -23,14 +16,43 @@
       value-field="item"
       text-field="name"
       disabled-field="notEnabled"
+      stacked
     ></b-form-radio-group>
     <!-- <div class="mt-3">Selected: <strong>{{ selected }}</strong></div> -->
 
        <b-button variant="primary" @click="createQuestion()">Create</b-button>
     </b-card>
 
-    <b-card title="Questions:">
-      <hr>
+    <b-card>
+      <!-- icon  -->
+       <b-dropdown
+         id="dropdown"
+         variant="outline-white"
+         text-align="right"
+        >
+      <template #button-content>
+        <b-icon
+          icon="three-dots-vertical"
+          aria-hidden="true"
+        ></b-icon>
+      </template>
+
+      <b-dropdown-item
+        variant="primary"
+        >Edit</b-dropdown-item
+      >
+      <b-dropdown-item
+        variant="danger"
+        >Delete</b-dropdown-item
+      >
+    </b-dropdown>
+    <!-- <b-card-title title="Questions:"/> -->
+    <!-- divider -->
+          <div class="divider my-2">
+            <div class="divider-text">
+              Questions
+            </div>
+          </div>
       <b-card->{{ ask }}</b-card->
       <b-card-text> </b-card-text>
     </b-card>
@@ -39,7 +61,7 @@
 
 <script>
 import {
-  BCard, BFormTextarea, BButton, BFormRadioGroup,
+  BCard, BFormTextarea, BButton, BFormRadioGroup, BIcon, BDropdownItem, BDropdown,
 } from 'bootstrap-vue'
 // import { Store } from '../store/users/index'
 
@@ -49,6 +71,10 @@ export default {
     BFormTextarea,
     BButton,
     BFormRadioGroup,
+    BIcon,
+    BDropdownItem,
+    BDropdown,
+    // BCardTitle,
     // BCardText,
     // BLink,
   },
@@ -86,7 +112,14 @@ button{
   resize: none;
   overflow: -moz-hidden-unscrollable;
 }
-.card{
+/* .card{
   border: 1px black;
+} */
+#dropdown{
+  /* border: 0 !important; */
+  border: none;
+  border-color: transparent;
+  box-shadow: none;
+  margin: 0px;
 }
 </style>
