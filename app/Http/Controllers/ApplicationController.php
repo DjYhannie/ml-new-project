37bee32a163748ee618a08841202f81ac03a01fd
Rhea May Ardiente <RHEA@MLHUILLIER.LOCAL>
2021-09-22 08:29:50 +0800

    Aplication Controller restored

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -4,18 +4,11 @@ namespace App\Http\Controllers;
 
 use App\User;
 use App\Http\Controllers\Controller;
-use GuzzleHttp\Psr7\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Validator;
-
-
-
 
 class ApplicationController extends Controller
 {
-    public function answerForm(Request $request)
+    public function index()
     {
-
+        return view('application');
     }
 }
