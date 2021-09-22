944628b401b424a5c4fa85e99797faffaf913871
Rhea May Ardiente <RHEA@MLHUILLIER.LOCAL>
2021-09-22 08:22:08 +0800

    changes

---
+++ app/Http/Controllers/ApplicationController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use App\Http\Controllers\Controller;
+use GuzzleHttp\Psr7\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+
+
+
+
+class ApplicationController extends Controller
+{
+    public function answerForm(Request $request)
+    {
+
+    }
+}
