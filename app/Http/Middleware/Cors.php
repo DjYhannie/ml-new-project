1e442e4a4a461a730aa27ea7de554c79e714f036
Joe John Ferrolino <joejohnferrolino@gmail.com>
2021-10-22 18:36:23 +0800

    Handle cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,6 +16,9 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
+        header_remove('Access-Control-Allow-Credentials');
+        header_remove('Access-Control-Allow-Origin');
+
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
