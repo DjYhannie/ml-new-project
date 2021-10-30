a761ac25f9407e23ad0861b41738c1c524f29ff1
Jan_Patrick Tancinco <janpatricktancinco@gmail.com>
2021-10-30 15:55:29 +0800

    hot fixes

--- server.js
+++ server.js
@@ -4,11 +4,6 @@ const serveStatic = require("serve-static")
 const path = require('path');
 const port = process.env.PORT || 3000;
 app = express();
-app.use(serveStatic(path.join(__dirname, '/dist')));
+app.use(serveStatic(__dirname + "/dist"));
 app.use(history());
-
-app.get('/', function (req, res) {
-    res.render(path.join(__dirname + '/dist/index.html'));
-});
-
 app.listen(port);
