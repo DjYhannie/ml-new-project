7af19e250dc7d6d002da6337986ff65a49414739
Jan_Patrick Tancinco <janpatricktancinco@gmail.com>
2021-10-30 15:18:26 +0800

    for deployment

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ var history = require('connect-history-api-fallback');
 const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
+const port = process.env.PORT || 3000;
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
 app.use(history());
-const port = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+    res.render(path.join(__dirname + '/dist/index.html'));
+});
+
 app.listen(port);
