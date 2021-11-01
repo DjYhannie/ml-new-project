7119a6558164b57ac7a18261d89a96512e0868bb
aparece1241 <62504360+aparece1241@users.noreply.github.com>
2021-11-01 17:49:17 +0800

    hot fixes

--- app/Http/Controllers/QuestionnaireController.php
+++ app/Http/Controllers/QuestionnaireController.php
@@ -203,7 +203,7 @@ class QuestionnaireController extends Controller
 
 
             return response()->json([
-                'data'      => $url_token->put('questionnaire', $questionnaire),
+                'data'      => $url_token['questionnaire'] = $questionnaire,
                 'url_token' => $url_token->token
             ]);
 
